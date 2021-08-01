854a1660c0943bdb1c9f901d33a5e031caa9b793
Diego Hernandez <redbandito@outlook.com>
2021-08-01 12:11:05 -0500

    Nuevo archivo para el ejercicio de tipos

---
+++ TSintro/src/ejercicios/01-basicTypes.ts
@@ -0,0 +1,11 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+let n: string = 'sum guy';
+let health: number | string= 12;
+let alive: boolean = true;
+
+n = 'Pepe';
+health='Full';
+console.log(n, health);
--- TSintro/src/index.ts
+++ TSintro/src/index.ts
@@ -2,10 +2,3 @@
     ===== Código de TypeScript =====
 */
 
-let n: string = 'sum guy';
-let health: number | string= 12;
-let alive: boolean = true;
-
-n = 'Pepe';
-health='Full';
-console.log(n, health);
