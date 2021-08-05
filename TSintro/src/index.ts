d3aa4a86ea312e2dcd3236d1fa4aa55913dffdfd
Diego Hernandez <redbandito@outlook.com>
2021-08-05 12:53:45 -0500

    Tercer ejercicio

--- TSintro/src/index.ts
+++ TSintro/src/index.ts
@@ -1,3 +1,40 @@
 /*
     ===== Código de TypeScript =====
 */
+
+function add (x:number, n:number)
+{
+    return x+n;
+}
+
+const arrowAdd = (a:number, b:number):number =>{
+    return a+b;
+}
+
+function multiply (num:number, other?:number, base:number=2):number
+{
+    return num*base;
+}
+
+interface ApexCharacter {
+    nombre: string;
+    pv:number;
+    showHp: ()=>void;
+}
+
+function heal(character:ApexCharacter, healing:number):void
+{
+    character.pv += healing;
+}
+
+//Declaración de objeto que hereda una interfaz
+const newCharacter:ApexCharacter={
+    nombre:"Wraith",
+    pv: 100,
+    showHp()
+    {
+        console.log('La vida es: '+ this.pv)
+    }
+}
+
+heal(newCharacter, 25);
