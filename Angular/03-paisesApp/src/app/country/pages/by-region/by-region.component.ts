d32c713ed280dfafba857fb063367ff16a4be573
Diego Hernandez <redbandito@outlook.com>
2021-08-25 07:43:12 -0500

    Feat: Functional country app (Not complete)

--- Angular/03-paisesApp/src/app/country/pages/by-region/by-region.component.ts
+++ Angular/03-paisesApp/src/app/country/pages/by-region/by-region.component.ts
@@ -1,16 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { Country } from '../../interfaces/country.interface';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-region',
   templateUrl: './by-region.component.html',
   styles: [
+    `
+      button {
+        margin-right: 5px;
+      }
+    `
   ]
 })
-export class ByRegionComponent implements OnInit {
+export class ByRegionComponent {
 
-  constructor() { }
+  regions: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania']; 
+  activeRegion: string = '';
+  countries: Country[] = [];
+  havingError: boolean=false;
+  constructor( private countryService:CountryService ) { }
 
-  ngOnInit(): void {
+  getCSSClass(region:string):string
+  {
+    return (region===this.activeRegion)? 'btn-primary': 'btn-outline-primary';
   }
+  activateRegion ( region:string ) {
+    
+    if(region===this.activeRegion)
+    {
+      return;
+    }
 
+    this.activeRegion=region;
+    this.havingError=false;
+    this.countries=[];
+    this.countryService.searchRegion(region)
+      .subscribe(countries => this.countries=countries);
+  }
+    
 }
--- Angular/03-paisesApp/src/app/country/services/country.service.ts
+++ Angular/03-paisesApp/src/app/country/services/country.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Country } from '../interfaces/country.interface';
@@ -9,21 +9,35 @@ import { Country } from '../interfaces/country.interface';
 export class CountryService {
 
   private apiUrl: string = 'https://restcountries.eu/rest/v2';
+
+  get httpParams () 
+  {
+    return new HttpParams ()
+      .set ('fields', 'name;capital;flag;alpha2Code;population')
+  }
+
   constructor( private http:HttpClient) { }
 
   searchCountry(term:string): Observable <Country[]>
   {
     const url = `${this.apiUrl}/name/${term}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, {params:this.httpParams});
   }
   searchCapital(term:string): Observable <Country[]>
   {
     const url = `${this.apiUrl}/capital/${term}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, {params:this.httpParams});
   }
   getCountryCode(id:string): Observable <Country>
   {
     const url = `${this.apiUrl}/alpha/${id}`;
     return this.http.get<Country>(url);
   }
+
+  searchRegion(region:string):Observable<Country[]>
+  {
+    const url = `${this.apiUrl}/region/${region}`;
+    return this.http.get<Country[]>(url, {params:this.httpParams});
+  }
+
 }
