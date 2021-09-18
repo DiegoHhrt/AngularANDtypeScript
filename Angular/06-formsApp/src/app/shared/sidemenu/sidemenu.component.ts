cd6bee9101c0881679cf394295fe6c2bd0b81cc5
Diego Hernandez <redbandito@outlook.com>
2021-09-18 15:57:50 -0500

    New app forms

---
+++ Angular/06-formsApp/src/app/shared/sidemenu/sidemenu.component.ts
@@ -0,0 +1,46 @@
+import { Component} from '@angular/core';
+
+interface MenuItem {
+  text: string;
+  route: string;
+}
+
+@Component({
+  selector: 'app-sidemenu',
+  templateUrl: './sidemenu.component.html',
+  styles: [
+  ]
+})
+export class SidemenuComponent{
+
+  templateMenu: MenuItem[] = [
+    {
+      text:'Basics',
+      route: './template/basics'
+    },
+    {
+      text:'Dynamics',
+      route: './template/dinamics'
+    },
+    {
+      text:'Switches',
+      route: './template/switches'
+    }
+  ]
+
+  reactiveMenu: MenuItem[] = [
+    {
+      text:'Basics',
+      route: './reactive/basics'
+    },
+    {
+      text:'Dynamics',
+      route: './reactive/dinamics'
+    },
+    {
+      text:'Switches',
+      route: './reactive/switches'
+    }
+  ]
+
+}
