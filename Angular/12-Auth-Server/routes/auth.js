e7b00d722c47f85b47b8e82e82533717245089d9
Diego Hernandez <redbandito@outlook.com>
2021-10-21 16:35:44 -0500

    End backend

--- Angular/12-Auth-Server/routes/auth.js
+++ Angular/12-Auth-Server/routes/auth.js
@@ -1,29 +1,29 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
+const { newUser, userLogin, tokenRenew } = require('../controllers/auth');
+const { fieldValidation } = require('../middlewares/fieldValidation');
+const { validateJWT } = require('../middlewares/validate-jwt');
 
 const router = Router();
 
 //New user creation
-router.post('/new', (req, resp) => {
-    return resp.json({
-        ok:true,
-        msg: "user creation /new"
-    })
-});
+router.post('/new', [
+    check('email', 'Email is not optional').isEmail(),
+    check('pw', 'Password is not optional').isLength({min:6}),
+    check('name', "Name is not optional").not().isEmpty(),
+    fieldValidation 
+], newUser);
 
 //User login
-router.post('/', (req, resp) => {
-    return resp.json({
-        ok:true,
-        msg: "user login/"
-    })
-});
+router.post('/', [
+    check('email', 'Email is not optional').isEmail(),
+    check('pw', 'Password is not optional').isLength({min:6}),
+    fieldValidation
+], userLogin);
 
 //Token validation
-router.get('/renew', (req, resp) => {
-    return resp.json({
-        ok:true,
-        msg: "Renew"
-    })
-});
+router.get('/renew', [
+    validateJWT
+], tokenRenew);
 
 module.exports = router;
